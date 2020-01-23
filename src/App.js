5284007358fa027a8108e6ae357c6aff8e9f0d4d
Michael Parry <crashvideoproductions@gmail.com>
2020-01-22 19:24:25 -0500

    update package.json for Github Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 
@@ -10,7 +10,7 @@ export default function App() {
         <Route path="/about">
           <About />
         </Route>
-        <Route path="/">
+        <Route path="/" exact>
           <Home />
         </Route>
       </Switch>
