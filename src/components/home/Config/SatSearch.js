e4cd92edf886af1e8df412429ae8024320b04329
Michael Parry <crashvideoproductions@gmail.com>
2020-01-23 13:04:20 -0500

    add satSearch search functionality

--- src/components/home/Config/SatSearch.js
+++ src/components/home/Config/SatSearch.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
+import sats from "../../../json/sats";
 
 export default class SatSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.state = {
+      value: "",
+      satFound: sats
+    };
+  }
+
+  handleSearch(e) {
+    this.setState({ value: e.target.value });
+    let query = e.target.value.toUpperCase();
+    let searchList = sats.filter(sat => sat.nickname.includes(query));
+    if (searchList.length == 0) {
+      searchList.push({ nickname: "None found..." });
+    }
+    this.setState({ satFound: searchList });
+  }
   render() {
+    const satList = this.state.satFound.map(sat => (
+      <a href="#0" className="list-group-item list-group-item-action bg-light">
+        {sat.nickname}
+      </a>
+    ));
+
     return (
       <div className="form-group">
         <label>Satellites</label>
@@ -11,43 +36,10 @@ export default class SatSearch extends Component {
             name="sat-search"
             className="list-group-item"
             placeholder="Search.."
+            value={this.state.value}
+            onChange={this.handleSearch}
           />
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light"
-          >
-            AO-92
-          </a>
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light"
-          >
-            AO-91
-          </a>
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light"
-          >
-            SO-50
-          </a>
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light"
-          >
-            PO-101
-          </a>
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light"
-          >
-            CAS-3H
-          </a>
-          <a
-            href="#0"
-            className="list-group-item list-group-item-action bg-light disabled"
-          >
-            AO-95
-          </a>
+          {satList}
         </div>
       </div>
     );
